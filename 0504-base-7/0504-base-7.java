6abb99f03feaa614bde11b62ed3cbb2152094a36
Mayank-117 <sharmamayank5490@gmail.com>
2026-07-07 16:11:45 +0530

    Time: 1 ms (69.79%), Space: 42.7 MB (59.94%) - LeetHub

---
+++ 0504-base-7/0504-base-7.java
@@ -0,0 +1,31 @@
+    class Solution {
+    public String convertToBase7(int num) {
+
+        if (num == 0) {
+            return "0";
+        }
+
+        boolean negative = num < 0;
+
+        num = Math.abs(num);
+
+        StringBuilder ans = new StringBuilder();
+
+        while (num > 0) {
+
+            int rem = num % 7;   
+
+            ans.append(rem);    
+
+            num = num / 7;       
+        }
+
+        if (negative) {
+            ans.append("-");
+        }
+
+        return ans.reverse().toString();
+    }
+}
+        
+    
